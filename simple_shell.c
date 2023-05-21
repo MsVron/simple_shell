02d9a3db023d79f8d2291bf982af5c3a88f0c02c
MsVron <126300537+MsVron@users.noreply.github.com>
2023-05-21 17:58:03 +0100

    simple shell?

--- simple_shell.c
+++ simple_shell.c
@@ -7,12 +7,27 @@
 
 #define BUFFER_SIZE 1024
 
+/**
+ *display_prompt - Displays a custom prompt
+ *
+ *Description: This function prints a custom prompt "$ " to the standard
+ *             output. It ensures that the prompt is immediately visible by
+ *             flushing the standard output buffer.
+ */
 void display_prompt(void)
 {
     printf("$ "); /* Custom prompt */
     fflush(stdout);
 }
 
+/**
+ * read_command - Reads a command from standard input
+ *
+ * Description: This function reads a command from standard input using fgets
+ *              and returns it as a dynamically allocated string.
+ *
+ * Return: A pointer to the dynamically allocated string containing the command.
+ */
 char *read_command(void)
 {
     char *command = malloc(BUFFER_SIZE);
@@ -20,6 +35,16 @@ char *read_command(void)
     return command;
 }
 
+/**
+ *execute_command - Executes a command
+ *
+ *@command: The command to execute
+ *
+ *Description: This function executes a command by forking a child process and
+ *             using execvp to run the command. If the command is "exit", it
+ *             exits the shell with a success message. It waits for the child
+ *             process to complete if it is the parent process.
+ */
 void execute_command(char *command)
 {
     command[strcspn(command, "\n")] = '\0'; /* Remove trailing newline character */
@@ -53,6 +78,26 @@ void execute_command(char *command)
     }
 }
 
+/**
+ *main - Entry point of the program
+ *
+ *@argc: The number of command-line arguments
+ *@argv: An array of command-line argument strings
+ *
+ *Description: The main function serves as the entry point of the program.
+ *             It provides two modes of operation: file mode and interactive
+ *             mode. In file mode, if a filename is provided as a command-
+ *             line argument, the program opens the file, reads each line,
+ *             and executes the commands found in the file. In interactive
+ *             mode, the program displays a prompt, reads commands from
+ *             standard input, and executes them. The program terminates
+ *             gracefully on encountering the end of file (Ctrl+D) in
+ *             interactive mode. The execution of commands is handled by
+ *             the execute_command function. BUFFER_SIZE is defined to
+ *             provide a consistent buffer size.
+ *
+ *Return: Always 0 (Success)
+ */
 int main(int argc, char *argv[])
 {
     char *filename = NULL;
