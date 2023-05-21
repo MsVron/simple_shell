f90199bb8dc567969c65f05770a5821d5d73af04
MsVron <126300537+MsVron@users.noreply.github.com>
2023-05-21 17:52:09 +0100

    beautiful betty code

---
+++ betty.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ *main - Entry point of the program
+ *
+ *Return: Always 0 (Success)
+ */
+int main(void)
+{
+	quote();
+	return (0);
+}
+
+/**
+ *quote - Prints an iconic girl programmer quote
+ */
+void quote(void)
+{
+	printf("I'm not a great programmer; I'm just a good programmer with great habits. - Ada Lovelace\n");
+}
